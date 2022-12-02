740cf51e9f3a73c23d537c1ded0f8ee9d33706f7
BoneShai <109730367+BoneShai@users.noreply.github.com>
2022-12-02 11:07:10 -0300

    Add files via upload

---
+++ _js/main.js
@@ -0,0 +1,12 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+
+  $('.slider-principal').slick({
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
